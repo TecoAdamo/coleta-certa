5fdb8ed9fe07cfbee84b20c9f5e41628943c368b
TecoAdamo <adamomateus478@gmail.com>
2026-05-17 20:19:03 -0300

    feat: validate neighborhood and transfer data to HomeViewController dynamically

--- ColetaCerta/Sources/Features/Address/AddressViewController.swift
+++ ColetaCerta/Sources/Features/Address/AddressViewController.swift
@@ -38,22 +38,43 @@ final class AddressViewController: UIViewController {
         
         let cep = addressView.cepInput.textField.text ?? ""
         let streetInput = addressView.streetInput.textField.text ?? ""
-        let numberInput = addressView.streetInput.textField.text ?? ""
-        let neighborhoodInput = addressView.streetInput.textField.text ?? ""
-        let cityInput = addressView.streetInput.textField.text ?? ""
-        let stateInput = addressView.streetInput.textField.text ?? ""
+        let numberInput = addressView.numericInput.textField.text ?? ""
+        let neighborhoodInput = addressView.neighborhoodInput.textField.text ?? ""
+        let cityInput = addressView.cityInput.textField.text ?? ""
+        let ufInput = addressView.ufInput.textField.text ?? ""
         
-        if cep.isEmpty || streetInput.isEmpty || numberInput.isEmpty || neighborhoodInput.isEmpty || cityInput.isEmpty || stateInput.isEmpty {
+        if cep.isEmpty || streetInput.isEmpty || numberInput.isEmpty || neighborhoodInput.isEmpty || cityInput.isEmpty || ufInput.isEmpty {
             let alert = UIAlertController(title: "Erro", message: "Preencha todos os campos", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
+        } else {
+            let service = CollectionService()
+            let allSchedules = service.loadSchedule()
+            
+            let neighborhoodExists = allSchedules.contains { $0.bairro.lowercased() == neighborhoodInput.lowercased() }
+            
+            if !neighborhoodExists {
+                let alert = UIAlertController(title: "Erro", message: "Bairro não encontrado", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            let nextViewController = HomeViewController()
+            
+            nextViewController.selectedSuburb = neighborhoodInput
+            
+            addressView.cepInput.textField.text = ""
+            addressView.streetInput.textField.text = ""
+            addressView.numericInput.textField.text = ""
+            addressView.neighborhoodInput.textField.text = ""
+            addressView.cityInput.textField.text = ""
+            addressView.ufInput.textField.text = ""
+            
+            navigationController?.pushViewController(
+                nextViewController,
+                animated: true
+            )
         }
-        
-        let nextViewController = HomeViewController()
-        
-        navigationController?.pushViewController(
-            nextViewController,
-            animated: true
-        )
     }
 }
--- ColetaCerta/Sources/Features/Home/HomeViewController.swift
+++ ColetaCerta/Sources/Features/Home/HomeViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    private let homeView = HomeView()
+    
+    private var tableItems: [HomeItem] = []
+    
+    var scheduleData: CollectionSchedule? {
+        didSet {
+            setupTableItems()
+        }
+    }
+    
+    var selectedSuburb: String?
+    
+    override func loadView() {
+        view = homeView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,9 +33,80 @@ final class HomeViewController: UIViewController {
         navigationController?.interactivePopGestureRecognizer?.delegate = nil
         
         setupUI()
+        setupTableView()
+        loadMockData()
     }
     
     private func setupUI(){
         view.backgroundColor = Colors.backgroundCardWhite
+    }
+    
+    private func setupTableView(){
+        homeView.tableView.register(
+            CollectionCell.self,
+            forCellReuseIdentifier: "CollectionCell"
+        )
+        homeView.tableView.dataSource = self
+    }
+    
+    private func setupTableItems() {
+        guard let season = scheduleData?.baixaTemporada else { return }
+        var items: [HomeItem] = []
+        
+        for dia in season.organica.dias {
+            items.append(HomeItem(
+                diaSemana: dia,
+                diaMes: "",
+                tipo: .organica,
+                horario: season.organica.inicio
+            ))
+        }
+    
+        for dia in season.seletiva.dias {
+            items.append(HomeItem(
+                diaSemana: dia,
+                diaMes: "",
+                tipo: .seletiva,
+                horario: season.seletiva.inicio
+            ))
+        }
+        
+        self.tableItems = items
+        
+        homeView.tableView.reloadData()
+    }
+    
+    private func loadMockData() {
+        
+        let service = CollectionService()
+        let allSchedules = service.loadSchedule()
+        
+        let suburbSearch = selectedSuburb
+        
+        if let matchedSchedule = allSchedules.first(where: {$0.bairro.lowercased() == suburbSearch?.lowercased()}) {
+            self.scheduleData = matchedSchedule
+        } else {
+            print("Aviso: O bairro '\(suburbSearch)' não foi encontrado no arquivo JSON.")
+        }
+            
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "CollectionCell",
+            for: indexPath
+        ) as! CollectionCell
+        
+        let item = tableItems[indexPath.row]
+        cell.configure(with: item)
+        
+        return cell
     }
 }
